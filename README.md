e543effb9cc6444149c4a97de88e5ef0ac43bce4
Muhammad Labeeb <muhammad.labeeb@192.168.10.58>
2025-04-23 19:08:46 +0500

    working prototype

---
+++ README.md
@@ -0,0 +1,22 @@
+# Raylib C Game
+
+A simple 2D bounce game built with [raylib](https://www.raylib.com/) in C. This project is set up to run on macOS with Apple Silicon (ARM64).
+
+---
+
+## Installation
+
+Make sure you build raylib from source:
+
+```
+git clone https://github.com/raysan5/raylib
+cd raylib
+cd src
+make
+```
+
+Copy `librarylib.a` `raylib.h` `raymath.h` files and paste them in the `lib` folder in the project's root.
+
+## Execution
+Run `make build_osx` to run the game.
+
---
+++ src/main.c
@@ -0,0 +1,104 @@
+#include <raylib.h>
+#include <stdio.h> // NULL
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+#define PIXEL_X 1280 // Screen width
+#define PIXEL_Y 800	 // Screen height
+#define BALL_RADIUS 40
+#define BALL_LEFT_LIMIT (BALL_RADIUS)
+#define BALL_RIGHT_LIMIT (PIXEL_X - BALL_RADIUS)
+#define JUMP_STRENGTH -25 // Lower number means a higher jump
+
+typedef struct {
+	float x, y, size, jump_strength, velocity;
+} Ball;
+
+typedef struct {
+	float x, y, width, height;
+} Platform;
+
+Platform *ball_on_platform(Ball *ball, Platform *platform, int platform_count) {
+	Vector2 center = {.x = (*ball).x, .y = (*ball).y};
+	for (int i = 0; i < platform_count; i++) {
+		Rectangle rectangle = *(Rectangle *)&platform[i];
+		if (CheckCollisionCircleRec(center, (*ball).size, rectangle)) {
+			return &platform[i];
+		}
+	}
+	return NULL;
+}
+
+void update_vertical_location(Ball *ball, Platform *platforms,
+							  int platform_count) {
+	Platform *on_platform = ball_on_platform(ball, platforms, platform_count);
+	if (on_platform && (*ball).velocity == 0) {
+		// If ball is on the platform with 0 velocity, do nothing
+		return;
+	} else {
+		// change balls y location based on current velocity
+		((*ball).velocity)++;
+		(*ball).y = (*ball).y + (*ball).velocity;
+		on_platform = ball_on_platform(ball, platforms, platform_count);
+		// If ball was coming down and it hits a platform, stop it
+		if (on_platform && (*ball).velocity > 0) {
+			(*ball).velocity = 0;
+			(*ball).y = (*on_platform).y - (*ball).size;
+		}
+	}
+}
+
+int main() {
+	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+
+	InitWindow(PIXEL_X, PIXEL_Y, "cgame");
+
+	Ball ball = {.x = PIXEL_X / 2,
+				 .y = PIXEL_Y / 2,
+				 .size = BALL_RADIUS,
+				 .jump_strength = JUMP_STRENGTH,
+				 .velocity = 0};
+
+	int platform_count = 2;
+	Platform platforms[platform_count];
+	platforms[0] =
+		(Platform){.x = 0, .y = PIXEL_Y - 20, .width = PIXEL_X, .height = 20};
+	platforms[1] =
+		(Platform){.x = 0, .y = PIXEL_Y - 200, .width = 400, .height = 20};
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		for (int i = 0; i < platform_count; i++) {
+			DrawRectangle(platforms[i].x, platforms[i].y, platforms[i].width,
+						  platforms[i].height, BLACK);
+		}
+
+		// Control horizontal movement
+		if (IsKeyDown(KEY_LEFT)) {
+			ball.x = MAX(BALL_LEFT_LIMIT, ball.x - 8);
+		}
+		if (IsKeyDown(KEY_RIGHT)) {
+			ball.x = MIN(BALL_RIGHT_LIMIT, ball.x + 8);
+		}
+
+		// Control vertical movement
+		if ((ball_on_platform(&ball, platforms, platform_count)) &&
+			IsKeyPressed(KEY_SPACE)) {
+			ball.velocity = JUMP_STRENGTH;
+		}
+		update_vertical_location(&ball, platforms, platform_count);
+
+		DrawCircle(ball.x, ball.y, ball.size, RED);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
+}
